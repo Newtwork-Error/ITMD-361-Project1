76f3ecd627621907eb29fd9541efb1d1815d1c9f
Newtwork-Error <77990528+Newtwork-Error@users.noreply.github.com>
2022-04-12 23:41:26 -0500

    Added the map page and changed the JavaScript to not show errors on wrong pages

--- js/script.js
+++ js/script.js
@@ -17,10 +17,21 @@ function init(){
   var harryPic1 = document.getElementById('harry-pic1');
   var harryPic2 = document.getElementById('harry-pic2');
   var daisyPic1 = document.getElementById('daisy-pic1');
-  miloPic1.addEventListener('click', petMilo);
-  miloPic2.addEventListener('click', petMilo);
-  harryPic1.addEventListener('click', petHarry);
-  harryPic2.addEventListener('click', petHarry);
-  daisyPic1.addEventListener('click', petDaisy);
+  if(miloPic1 != null){
+    miloPic1.addEventListener('click', petMilo);
+    miloPic2.addEventListener('click', petMilo);
+    harryPic1.addEventListener('click', petHarry);
+    harryPic2.addEventListener('click', petHarry);
+    daisyPic1.addEventListener('click', petDaisy);
+  }
 }
+
+function initMap(){
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 8
+  });
+}
+
+google.maps.event.addDomListener(window, 'load', initMap);
 window.addEventListener('load', init);
